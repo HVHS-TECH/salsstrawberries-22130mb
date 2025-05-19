d16ad5d9b722f0f16e2bdf092d23edda3b1f873c
22130mb <22130mb@hvhs.school.nz>
2025-05-19 00:26:38 +0000

    .

--- main.mjs
+++ main.mjs
@@ -1,1 +1,27 @@
+/**************************************************************/
+// main.mjs
+// Main entry for index.html
+// Written by <Your Name Here>, Term 2 202?
+/**************************************************************/
+const COL_C = 'white';	    // These two const are part of the coloured 	
+const COL_B = '#CD7F32';	//  console.log for functions scheme
+console.log('%c main.mjs', 
+    'color: blue; background-color: white;');
+
+/**************************************************************/
+// Import all external constants & functions required
+/**************************************************************/
+// Import all the constants & functions required from fb_io module
+import { fb_authenticate, fb_writeto } from './script.mjs';
+window.fb_authenticate = fb_authenticate;
+window.fb_writeto = fb_writeto;
+
+/**************************************************************/
+// index.html main code
+/**************************************************************/
+
+
+/**************************************************************/
+//   END OF CODE
+/**************************************************************/
 
--- script.mjs
+++ script.mjs
@@ -1,1 +1,84 @@
+//**************************************************************/
+// fb_io.mjs
+// Generalised firebase routines
+// Written by <Your Name Here>, Term 2 202?
+//
+// All variables & function begin with fb_  all const with FB_
 
+// Diagnostic code lines have a comment appended to them //DIAG
+/**************************************************************/
+const COL_C = 'white';	    // These two const are part of the coloured 	
+const COL_B = '#CD7F32';	//  console.log for functions scheme
+console.log('%c scrip.mjs',
+    'color: blue; background-color: white;');
+
+/**************************************************************/
+// Import all external constants & functions required
+/**************************************************************/
+// Import all the methods you want to call from the firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { ref, set } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+/**************************************************************/
+// EXPORT FUNCTIONS
+var fb_gameDB;
+// List all the functions called by code or html outside of this module
+function fb_authenticate() {
+    console.log('%c fb_initialise(): ', 'color: ' + COL_C + '; background-color: ' + COL_B + ';');
+    console.log("hello world");
+    const FB_GAMECONFIG = {
+        apiKey: "AIzaSyCHDtQ5nuCxgp_XCL_RtR7YVHv8mO1rhmc",
+        authDomain: "comp-2025-max-bergman-4bb13.firebaseapp.com",
+        databaseURL: "https://comp-2025-max-bergman-4bb13-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "comp-2025-max-bergman-4bb13",
+        storageBucket: "comp-2025-max-bergman-4bb13.firebasestorage.app",
+        messagingSenderId: "75891205088",
+        appId: "1:75891205088:web:9ce6dd10fe8f59fb6f8185",
+        measurementId: "G-860HVWZ49V"
+    };
+
+    const FB_GAMEAPP = initializeApp(FB_GAMECONFIG);
+    fb_gameDB = getDatabase(FB_GAMEAPP);
+
+    console.info(fb_gameDB);
+
+
+
+    const AUTH = getAuth();
+    const PROVIDER = new GoogleAuthProvider();
+
+    // The following makes Google ask the user to select the account
+
+    PROVIDER.setCustomParameters({
+
+        prompt: 'select_account'
+    });
+    signInWithPopup(AUTH, PROVIDER).then((result) => {
+    })
+        .catch((error) => {
+            console.log(error);
+        });
+    }
+
+ function fb_writeto(){
+    const dbReference = ref(fb_gameDB, "Users/UserID");
+    var UserInformation = { highscore: 50, Name: "person" };
+    set(dbReference, UserInformation).then(() => {
+
+        console.log("written the following indformation to the database");
+        console.log(UserInformation);
+    }).catch((error) => {
+
+        console.log("write error");
+        console.log(error);
+    });
+}
+    
+    
+    
+    export {
+ 
+  fb_authenticate,
+  fb_writeto
+};
